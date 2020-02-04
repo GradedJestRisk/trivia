e13af38aa33fd56a343479a7e4de1c7f3304e1ab
TOP Pierre (TOPI) <ptop@octo.com>
2020-02-04 16:37:54 +0100

    add tests for questions categories

--- javascript/spec/game.spec.js
+++ javascript/spec/game.spec.js
@@ -3,7 +3,7 @@
 const expect = require('chai').expect
 const Game = require('../game.js')
 
-describe('setting up', () => {
+describe('BUG ?!', () => {
 
   it('should not allow single player (isPlayable not used) - bug ?', () => {
     var game = new Game();
@@ -15,6 +15,14 @@ describe('setting up', () => {
     expect(messages[1]).to.equal("They are player number 1")
   })
 
+  it("should not to roll 0 ", ()=>{
+    var game = new Game();
+    game.add('Chet')
+    game.add('Suzan')
+
+    game.roll(0)
+    expect(true).to.be(true)
+  })
 })
 
 describe('playing', () => {
@@ -44,6 +52,18 @@ describe('playing', () => {
       const positionMessage = messages[messages.length - 3]
       expect(positionMessage).to.equal("Chet's new location is 2")
     })
+    it(' should ask a question', () => {
+      var game = new Game();
+      game.add('Chet')
+      game.add('Suzan')
+
+      game.roll(2)
+      const messages = game.getMessages()
+      //console.log(messages)
+
+      const questionMessage = messages[messages.length - 1]
+      expect(questionMessage).to.match(/Question/)
+    })
   })
 
   /*
@@ -52,11 +72,125 @@ describe('playing', () => {
   2 'Suzan was added',
   3 'They are player number 2',
   4 'Chet is the current player',
-  5 'They have rolled a 2',      -2
-  6 "Chet's new location is 2",  -1
-  7 'The category is Sports',     0
-  8 'Sports Question 0'          +1
+  5 'They have rolled a 2',      -3
+  6 "Chet's new location is 2",  -2
+  7 'The category is Sports',     -1
+  8 'Sports Question 0'           0
   */
 
 
+})
+
+describe("according to position, should determine the question's category", () => {
+  describe("Pop category",() =>{
+    let game;
+    beforeEach(() => {
+      game = new Game();
+      game.add('Chet')
+      game.add('Suzan')
+    })
+
+    it("when the position is 0 ", ()=>{
+      game.roll(0) // BUG
+      const messages = game.getMessages()
+      const questionMessage = messages[messages.length - 1]
+      expect(questionMessage).to.match(/Pop Question/)
+    })
+
+    it("when the position is 4", ()=>{
+      game.roll(4)
+      const messages = game.getMessages()
+      const questionMessage = messages[messages.length - 1]
+      expect(questionMessage).to.match(/Pop Question/)
+    })
+
+    it("when the position is 8", ()=>{
+      game.roll(8)
+      const messages = game.getMessages()
+      const questionMessage = messages[messages.length - 1]
+      expect(questionMessage).to.match(/Pop Question/)
+    })
+  })
+
+  describe("Science category",() =>{
+    let game;
+    beforeEach(() => {
+      game = new Game();
+      game.add('Chet')
+      game.add('Suzan')
+    })
+
+    it("when the position is 1 ", ()=>{
+      game.roll(1)
+      const messages = game.getMessages()
+      const questionMessage = messages[messages.length - 1]
+      expect(questionMessage).to.match(/Science Question/)
+    })
+
+    it("when the position is 5", ()=>{
+      game.roll(5)
+      const messages = game.getMessages()
+      const questionMessage = messages[messages.length - 1]
+      expect(questionMessage).to.match(/Science Question/)
+    })
+
+    it("when the position is 9", ()=>{
+      game.roll(9)
+      const messages = game.getMessages()
+      const questionMessage = messages[messages.length - 1]
+      expect(questionMessage).to.match(/Science Question/)
+    })
+  })
+
+  describe("Sport category",() =>{
+    let game;
+    beforeEach(() => {
+      game = new Game();
+      game.add('Chet')
+      game.add('Suzan')
+    })
+
+    it("when the position is 2 ", ()=>{
+      game.roll(2)
+      const messages = game.getMessages()
+      const questionMessage = messages[messages.length - 1]
+      expect(questionMessage).to.match(/Sports Question/)
+    })
+
+    it("when the position is 6", ()=>{
+      game.roll(6)
+      const messages = game.getMessages()
+      const questionMessage = messages[messages.length - 1]
+      expect(questionMessage).to.match(/Sports Question/)
+    })
+
+    it("when the position is 10", ()=>{
+      game.roll(10)
+      const messages = game.getMessages()
+      const questionMessage = messages[messages.length - 1]
+      expect(questionMessage).to.match(/Sports Question/)
+    })
+  })
+  describe("Rock category",() => {
+    let game;
+    beforeEach(() => {
+      game = new Game();
+      game.add('Chet')
+      game.add('Suzan')
+    })
+
+    it("when the position is 3 ", () => {
+      game.roll(3)
+      const messages = game.getMessages()
+      const questionMessage = messages[messages.length - 1]
+      expect(questionMessage).to.match(/Rock Question/)
+    })
+
+    it("when the position is 7", () => {
+      game.roll(7)
+      const messages = game.getMessages()
+      const questionMessage = messages[messages.length - 1]
+      expect(questionMessage).to.match(/Rock Question/)
+    })
+  })
 })
