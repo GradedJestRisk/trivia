138b72d71e680fe126c4ec75eaacceadf9fee488
TOP Pierre (TOPI) <ptop@octo.com>
2020-02-05 10:36:54 +0100

    add the final test

--- javascript/spec/game.spec.js
+++ javascript/spec/game.spec.js
@@ -4,8 +4,7 @@ const expect = require('chai').expect
 const Game = require('../game.js')
 
 describe('BUG ?!', () => {
-
-  it('should not allow single player (isPlayable not used) - bug ?', () => {
+  it('allow single player (isPlayable not used)', () => {
     var game = new Game();
     game.add('Chet');
     game.roll(12)
@@ -14,31 +13,75 @@ describe('BUG ?!', () => {
     const messages = game.getMessages()
     expect(messages[1]).to.equal("They are player number 1")
   })
-
-  it("should not to roll 0 ", ()=>{
+  it("roll 0 is allowed ", ()=>{
     var game = new Game();
     game.add('Chet')
     game.add('Suzan')
 
     game.roll(0)
-    expect(true).to.be(true)
+    expect(true).to.equal(true)
+  })
+  it("should change player if the current player is still in the penalty box", ()=>{
+      const game = new Game();
+      game.add('Chet')
+      game.add('Henry')
+      //chet qui joue
+      game.roll(3)
+      game.wrongAnswer()
+      //henry qui joue
+      game.roll(1)
+      game.wasCorrectlyAnswered()
+      // chet qui essaye de sortir
+      game.roll(2)
+      //chet failed
+      // henry joue
+      game.roll(3)
+      const messages = game.getMessages()
+      expect(messages[messages.length-4]).to.equal('Chet is getting out of the penalty box')
+  })
+  describe('winning or losing; wasCorrectlyAnswered', ()=>{
+    it("returns true when the player does not win ", ()=>{
+      const game = new Game();
+      game.add('Jackie')
+      game.roll(5)
+      const finalResult = game.wasCorrectlyAnswered()
+      expect(finalResult).to.equal(true)
+    })
+    it("returns false when the player does win ", ()=>{
+      const game = new Game();
+      game.add('Jackie')
+      game.roll(5)
+      game.wasCorrectlyAnswered()
+      game.roll(3)
+      game.wasCorrectlyAnswered()
+      game.roll(3)
+      game.wasCorrectlyAnswered()
+      game.roll(3)
+      game.wasCorrectlyAnswered()
+      game.roll(3)
+      game.wasCorrectlyAnswered()
+      game.roll(3)
+      const finalResult = game.wasCorrectlyAnswered()
+      expect(finalResult).to.equal(false)
+    })
+  })
+  describe('a player can join the game anytime', ()=>{
+    it("returns true when the player does not win ", ()=>{
+      const game = new Game();
+      game.add('Jackie')
+      game.roll(5)
+      game.add('Susie')
+      const finalResult = game.wasCorrectlyAnswered()
+     const messages = game.getMessages()
+
+      const playerAddedMessage = messages[messages.length - 4]
+      expect(playerAddedMessage).to.equal("Susie was added")
+
+    })
   })
 })
 
-
 describe('playing', () => {
-
-  it('template', () => {
-    var game = new Game();
-    game.add('Chet');
-    game.roll(12)
-
-    const situation =  game.wasCorrectlyAnswered()
-    const messages = game.getMessages()
-
-    expect(messages[0]).to.equal("Chet was added")
-  })
-
   describe('after dice have been rolled', () => {
     it(' should go to position N if N is rolled', () => {
       var game = new Game();
@@ -47,7 +90,6 @@ describe('playing', () => {
 
       game.roll(2)
       const messages = game.getMessages()
-      console.log(messages)
 
       const positionMessage = messages[messages.length - 3]
       expect(positionMessage).to.equal("Chet's new location is 2")
@@ -59,7 +101,6 @@ describe('playing', () => {
 
       game.roll(12)
       const messages = game.getMessages()
-      console.log(messages)
 
       const positionMessage = messages[messages.length - 3]
       expect(positionMessage).to.equal("Chet's new location is 0")
@@ -71,7 +112,6 @@ describe('playing', () => {
 
       game.roll(13)
       const messages = game.getMessages()
-      console.log(messages)
 
       const positionMessage = messages[messages.length - 3]
       expect(positionMessage).to.equal("Chet's new location is 1")
@@ -83,40 +123,10 @@ describe('playing', () => {
 
       game.roll(2)
       const messages = game.getMessages()
-      //console.log(messages)
-
       const questionMessage = messages[messages.length - 1]
       expect(questionMessage).to.match(/Question/)
     })
   })
-
-  describe('when a player answers a question', ()=>{
-    let game;
-    beforeEach(() => {
-      game = new Game();
-      game.add('Chet')
-      game.add('Suzan')
-    })
-    describe('if the answer is wrong', ()=>{
-      it("should go to the penalty box", ()=>{
-        game.roll(1)
-        game.wrongAnswer()
-        const messages = game.getMessages()
-        const penaltyMessage = messages[messages.length - 1]
-        expect(penaltyMessage).to.equal('Chet was sent to the penalty box')
-      })
-    })
-    describe('if the answer is right', ()=>{
-      it("should gain 1 coin", ()=>{
-        game.roll(1)
-        game.wasCorrectlyAnswered()
-        const messages = game.getMessages()
-        const penaltyMessage = messages[messages.length - 1]
-        expect(penaltyMessage).to.equal('Chet now has 1 Gold Coins.')
-      })
-    })
-  })
-
   describe('when the player is in the penalty box', ()=>{
     let game;
     beforeEach(() => {
@@ -171,38 +181,21 @@ describe('playing', () => {
         const messages = game.getMessages()
         expect(messages[messages.length-1]).to.equal('Chet is not getting out of the penalty box')
       })
+
     })
   })
-
-
-
-  /*
-  0 Chet was added',
-  1 'They are player number 1',
-  2 'Suzan was added',
-  3 'They are player number 2',
-  4 'Chet is the current player',
-  5 'They have rolled a 2',      -3
-  6 "Chet's new location is 2",  -2
-  7 'The category is Sports',     -1
-  8 'Sports Question 0'           0
-  */
-
-  describe('setup rules', ()=>{
+  describe('joining game', ()=>{
     describe('after a player has ben added', () =>{
       it('should mention the player', () => {
         let game = new Game();
         game.add('Chet');
         const messages = game.getMessages()
-        console.log(messages)
         expect(messages[0]).to.equal('Chet was added')
       })
     })
   })
-
   describe('multi-player rules', ()=>{
     let game;
-
     describe('after a player has played', () =>{
       beforeEach(() => {
         game = new Game();
@@ -219,136 +212,145 @@ describe('playing', () => {
         expect(messages[messages.length - 5]).to.equal('Rosie is the current player')
       })
     })
-    it("wasCorrectlyAnswered returns false when the player won ", ()=>{
-      const game = new Game();
-      game.add('Jackie')
-      game.roll(5)
-      game.wasCorrectlyAnswered()
-      game.roll(3)
-      game.wasCorrectlyAnswered()
-      game.roll(3)
-      game.wasCorrectlyAnswered()
-      game.roll(3)
-      game.wasCorrectlyAnswered()
-      game.roll(3)
-      game.wasCorrectlyAnswered()
-      game.roll(3)
-      const finalResult = game.wasCorrectlyAnswered()
-      expect(finalResult).to.equal(false)
-    })
   })
 })
 
-describe("according to position, should determine the question's category", () => {
-  describe("Pop category",() =>{
-    let game;
-    beforeEach(() => {
-      game = new Game();
-      game.add('Chet')
-      game.add('Suzan')
-    })
+describe("questions", () => {
+  describe("according to position, should determine the question's category", () => {
+    describe("Pop category", () => {
+      let game;
+      beforeEach(() => {
+        game = new Game();
+        game.add('Chet')
+        game.add('Suzan')
+      })
 
-    it("when the position is 0 ", ()=>{
-      game.roll(0) // BUG
-      const messages = game.getMessages()
-      const questionMessage = messages[messages.length - 1]
-      expect(questionMessage).to.match(/Pop Question/)
-    })
+      it("when the position is 0 ", () => {
+        game.roll(0) // BUG
+        const messages = game.getMessages()
+        const questionMessage = messages[messages.length - 1]
+        expect(questionMessage).to.match(/Pop Question/)
+      })
 
-    it("when the position is 4", ()=>{
-      game.roll(4)
-      const messages = game.getMessages()
-      const questionMessage = messages[messages.length - 1]
-      expect(questionMessage).to.match(/Pop Question/)
-    })
+      it("when the position is 4", () => {
+        game.roll(4)
+        const messages = game.getMessages()
+        const questionMessage = messages[messages.length - 1]
+        expect(questionMessage).to.match(/Pop Question/)
+      })
 
-    it("when the position is 8", ()=>{
-      game.roll(8)
-      const messages = game.getMessages()
-      const questionMessage = messages[messages.length - 1]
-      expect(questionMessage).to.match(/Pop Question/)
+      it("when the position is 8", () => {
+        game.roll(8)
+        const messages = game.getMessages()
+        const questionMessage = messages[messages.length - 1]
+        expect(questionMessage).to.match(/Pop Question/)
+      })
+    })
+    describe("Science category", () => {
+      let game;
+      beforeEach(() => {
+        game = new Game();
+        game.add('Chet')
+        game.add('Suzan')
+      })
+
+      it("when the position is 1 ", () => {
+        game.roll(1)
+        const messages = game.getMessages()
+        const questionMessage = messages[messages.length - 1]
+        expect(questionMessage).to.match(/Science Question/)
+      })
+
+      it("when the position is 5", () => {
+        game.roll(5)
+        const messages = game.getMessages()
+        const questionMessage = messages[messages.length - 1]
+        expect(questionMessage).to.match(/Science Question/)
+      })
+
+      it("when the position is 9", () => {
+        game.roll(9)
+        const messages = game.getMessages()
+        const questionMessage = messages[messages.length - 1]
+        expect(questionMessage).to.match(/Science Question/)
+      })
+    })
+    describe("Sport category", () => {
+      let game;
+      beforeEach(() => {
+        game = new Game();
+        game.add('Chet')
+        game.add('Suzan')
+      })
+
+      it("when the position is 2 ", () => {
+        game.roll(2)
+        const messages = game.getMessages()
+        const questionMessage = messages[messages.length - 1]
+        expect(questionMessage).to.match(/Sports Question/)
+      })
+
+      it("when the position is 6", () => {
+        game.roll(6)
+        const messages = game.getMessages()
+        const questionMessage = messages[messages.length - 1]
+        expect(questionMessage).to.match(/Sports Question/)
+      })
+
+      it("when the position is 10", () => {
+        game.roll(10)
+        const messages = game.getMessages()
+        const questionMessage = messages[messages.length - 1]
+        expect(questionMessage).to.match(/Sports Question/)
+      })
+    })
+    describe("Rock category", () => {
+      let game;
+      beforeEach(() => {
+        game = new Game();
+        game.add('Chet')
+        game.add('Suzan')
+      })
+
+      it("when the position is 3 ", () => {
+        game.roll(3)
+        const messages = game.getMessages()
+        const questionMessage = messages[messages.length - 1]
+        expect(questionMessage).to.match(/Rock Question/)
+      })
+
+      it("when the position is 7", () => {
+        game.roll(7)
+        const messages = game.getMessages()
+        const questionMessage = messages[messages.length - 1]
+        expect(questionMessage).to.match(/Rock Question/)
+      })
     })
   })
-
-  describe("Science category",() =>{
+  describe('when a player answers a question', ()=>{
     let game;
     beforeEach(() => {
       game = new Game();
       game.add('Chet')
       game.add('Suzan')
     })
-
-    it("when the position is 1 ", ()=>{
-      game.roll(1)
-      const messages = game.getMessages()
-      const questionMessage = messages[messages.length - 1]
-      expect(questionMessage).to.match(/Science Question/)
+    describe('if the answer is wrong', ()=>{
+      it("should go to the penalty box", ()=>{
+        game.roll(1)
+        game.wrongAnswer()
+        const messages = game.getMessages()
+        const penaltyMessage = messages[messages.length - 1]
+        expect(penaltyMessage).to.equal('Chet was sent to the penalty box')
+      })
     })
-
-    it("when the position is 5", ()=>{
-      game.roll(5)
-      const messages = game.getMessages()
-      const questionMessage = messages[messages.length - 1]
-      expect(questionMessage).to.match(/Science Question/)
-    })
-
-    it("when the position is 9", ()=>{
-      game.roll(9)
-      const messages = game.getMessages()
-      const questionMessage = messages[messages.length - 1]
-      expect(questionMessage).to.match(/Science Question/)
-    })
-  })
-
-  describe("Sport category",() =>{
-    let game;
-    beforeEach(() => {
-      game = new Game();
-      game.add('Chet')
-      game.add('Suzan')
-    })
-
-    it("when the position is 2 ", ()=>{
-      game.roll(2)
-      const messages = game.getMessages()
-      const questionMessage = messages[messages.length - 1]
-      expect(questionMessage).to.match(/Sports Question/)
-    })
-
-    it("when the position is 6", ()=>{
-      game.roll(6)
-      const messages = game.getMessages()
-      const questionMessage = messages[messages.length - 1]
-      expect(questionMessage).to.match(/Sports Question/)
-    })
-
-    it("when the position is 10", ()=>{
-      game.roll(10)
-      const messages = game.getMessages()
-      const questionMessage = messages[messages.length - 1]
-      expect(questionMessage).to.match(/Sports Question/)
-    })
-  })
-  describe("Rock category",() => {
-    let game;
-    beforeEach(() => {
-      game = new Game();
-      game.add('Chet')
-      game.add('Suzan')
-    })
-
-    it("when the position is 3 ", () => {
-      game.roll(3)
-      const messages = game.getMessages()
-      const questionMessage = messages[messages.length - 1]
-      expect(questionMessage).to.match(/Rock Question/)
-    })
-
-    it("when the position is 7", () => {
-      game.roll(7)
-      const messages = game.getMessages()
-      const questionMessage = messages[messages.length - 1]
-      expect(questionMessage).to.match(/Rock Question/)
+    describe('if the answer is right', ()=>{
+      it("should gain 1 coin", ()=>{
+        game.roll(1)
+        game.wasCorrectlyAnswered()
+        const messages = game.getMessages()
+        const penaltyMessage = messages[messages.length - 1]
+        expect(penaltyMessage).to.equal('Chet now has 1 Gold Coins.')
+      })
     })
   })
 })
@@ -364,7 +366,6 @@ describe("purse persistence", ()=> {
       game.wasCorrectlyAnswered()
 
       const messages = game.getMessages()
-      console.log(messages)
       expect(messages.pop()).to.equal("Jackie now has 2 Gold Coins.")
     })
   })
